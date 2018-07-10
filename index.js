d889c112c36a946f76d29dc61c194b8db01f523f
Alyssa Taylor <alyssa.taylor@hlcsk12.net>
2018-07-10 20:31:36 +0000

    Working Add todo button

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
- $("#add-new-to-do-button"). click(function(){
+ $("#add-new-todo-button"). click(function(){
     var todoBlock = $(this).closest("#new-todo-block")
     var description = todoBlock.find("#new-todo-description")
     var pomodoroEstimate = todoBlock.find("#new-pomo-estimate")
-    $("ul").append("<li><input type = 'checkbox'/>" + "<span class= 'item'>" + description.val() + "</span>" + "<span class= 'pomo'>" + pomodoroEstimate.val() + "</span></li>")
+    $("ul").append("<li> <input type = 'checkbox'/> " + description.val() + " <span class= 'pomo'> " + pomodoroEstimate.val() + " </span> </li> ")
     
+    description.val("")
+    pomodoroEstimate.val("")
     
     
     
