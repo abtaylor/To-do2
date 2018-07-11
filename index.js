c2b08c5025be8e7e012d5fd7c5a2563e32473e4e
Alyssa Taylor <alyssa.taylor@hlcsk12.net>
2018-07-11 01:50:26 +0000

    Design for index finished

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ $(document).ready(function(){
     var todoBlock = $(this).closest("#new-todo-block")
     var description = todoBlock.find("#new-todo-description")
     var pomodoroEstimate = todoBlock.find("#new-pomo-estimate")
-    $("ul").append("<li> <input type = 'checkbox'/> " + description.val() + " <span class= 'pomo'> " + pomodoroEstimate.val() + " </span> </li> ")
-    
+    $("ul").append("<li> <input type= 'checkbox'/> " + description.val() + " <span class = 'pomo'> " + pomodoroEstimate.val() + " </span> </li> ")
+
     description.val("")
     pomodoroEstimate.val("")
-    
-    
-    
-    
  })   
 })
